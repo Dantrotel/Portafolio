d053f1da8e1686239cea90ddbf0179d637900446
Dantrottel <daniel.aguayo2001@alumnos.ubiobio.cl>
2025-05-07 13:24:31 -0400

    portafolio v1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About(){
     return (
         <section className="section">
           <h2>Acerca de mí</h2>
-          <p>Soy estudiante de la Universidad del Bío-Bío, apasionado por la tecnología, el desarrollo web y la automatización. Me gusta aprender y crear soluciones que ayuden a los demás.</p>
+          <p>Soy estudiante de la Universidad del Bío-Bío, apasionado por la tecnología, el desarrollo web.</p>
         </section>
       )
 }
